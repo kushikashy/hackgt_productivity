6dee3a4cf623f517bf696adfdd67e992f510f2a6
Kushi Kashyap <kushikash06@gmail.com>
2025-09-26 20:39:42 -0400

    added xcode project

---
+++ hackgt_productivity/hackgt_productivity/hackgt_productivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackgt_productivityApp.swift
+//  hackgt_productivity
+//
+//  Created by Kushi Kashyap on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct hackgt_productivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
