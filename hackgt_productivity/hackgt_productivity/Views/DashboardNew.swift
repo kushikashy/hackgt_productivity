2371ea2a6603268db5662bbf04de85b4c7d044c0
Kushi Kashyap <kushikash06@gmail.com>
2025-09-26 23:05:33 -0400

    added dashboard

---
+++ hackgt_productivity/hackgt_productivity/Views/DashboardNew.swift
@@ -0,0 +1,43 @@
+//
+//  DashboardNew.swift
+//  hackgt_productivity
+//
+//  Created by Kushi Kashyap on 9/26/25.
+//
+
+import SwiftUI
+
+struct DashboardNew: View {
+    @State private var name: String = "No name set"
+    @State private var authVM = AuthenticationVM()
+    
+    var body: some View {
+        VStack {
+            NavigationStack {
+                VStack {
+                    Text(name == "No name set" ? "Dashboard" : "\(name)'s Dashboard")
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading], 10)
+                        .padding(.bottom, 5)
+                        .bold()
+//                    NavigationLink(destination: projectDetails()) {
+//                                        Label("Add Project", systemImage: "plus")
+//                                            .font(.headline)
+//                                            .foregroundColor(.white)
+//                                            .padding()
+//                                            .background(Color.blue)
+//                                            .cornerRadius(10)
+//                                    }
+                }
+                Spacer()
+            }
+        }
+        .padding()
+        Spacer()
+    }
+}
+
+#Preview {
+    DashboardNew()
+}
---
+++ hackgt_productivity/hackgt_productivity/Views/Tasks.swift
@@ -0,0 +1,7 @@
+//
+//  Tasks.swift
+//  hackgt_productivity
+//
+//  Created by Kushi Kashyap on 9/26/25.
+//
+
